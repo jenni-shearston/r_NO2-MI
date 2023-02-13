e7ed58f6be4c4f46ca5fc4a0c93e889a873ac4ba
Jenni Shearston <jennishearston@jennis-mbp-2.dyn.nyp.org>
2023-02-13 11:35:07 -0500

    smoothing contour plot

--- Scripts/3_05_create_no2_hourly_contourplot.R
+++ Scripts/3_05_create_no2_hourly_contourplot.R
@@ -52,27 +52,52 @@ options(scipen = 999)
 #### 1: Create contour plot of NO2 and time of day #### 
 ####***************************************************
 
-# 1a Prepare dataset
+# 1a Make dataframe containing all unique datetime-city-no2 observations 
 df_contour <- data4casecross_city %>% 
-  dplyr::select(DayDateTime, no2_lag00) %>% 
-  distinct(DayDateTime, .keep_all = TRUE) %>% 
+  dplyr::select(DayDateTime, no2_lag00, city) %>% 
+  distinct() %>% 
+  dplyr::select(-city)
+
+# 1x Create y axis bin (NO2 conc bins) and z variable (count of hours for given
+#    NO2 conc bin and hour)
+df_contour <- df_contour %>% 
   mutate(hour = hour(DayDateTime),
-         no2_lag00 = round(no2_lag00, digits = 0)) %>% 
+         no2_lag00 = 5 * round(no2_lag00/5)) %>%
   group_by(hour, no2_lag00) %>% 
   summarise(count_no2 = n())
 
-# 1b Create and save plot
+# 1b Create case-complete grid with all possible hour and NO2 concentration
+#    combinations
+no2_max = max(df_contour$no2_lag00, na.rm = T)
+cc <- 
+  tibble(no2_lag00 = rep(seq(from = 0, to = no2_max, by = 5), times = 24),
+         hour = rep(seq(from = 0, to = 23), each = (no2_max/5)+1))
+
+# 1c Merge case-complete with observed data for plotting
+cc <- cc %>% 
+  left_join(df_contour, by = c('no2_lag00', 'hour'))
+
+# 1d Convert missing counts to 0 values
+#    If there is no count for a specific hour/conc combo, this means
+#    there are 0 observations of this
+cc <- cc %>%
+  mutate(count_no2 = ifelse(is.na(count_no2), 0, count_no2))
+         
+# 1e Create and save plot
 tiff(paste0(output_path, 'Plots/', 'fig2_no2_contourplot.tif'),
      units = "in", width = 12, height = 8, res = 300)
 
-df_contour %>% 
+cc %>% 
   ggplot(aes(x = hour, y = no2_lag00)) +
   stat_contour_filled(aes(z = count_no2)) +
-  scale_fill_distiller(super = metR::ScaleDiscretised, palette = "RdYlBu") +
+  scale_fill_distiller(super = metR::ScaleDiscretised, palette = "RdYlBu",
+                       breaks = c(0, 400, 800, 1200, 1600, 2000, 2400, 2800)) +
   labs(fill = 'Count of \n Hours') +
   xlab("Hour of Day") + ylab(expression('NO'[2]*' (ppb)')) +
   scale_x_continuous(limits = c(0, 23)) +
-  theme_bw(base_size = 20)
+  scale_y_continuous(limits = c(0, 60)) +
+  theme_bw(base_size = 16) +
+  theme(legend.text = element_text(size = 10))
 
 dev.off()
 
